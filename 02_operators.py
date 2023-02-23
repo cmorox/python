c925220e4bf26e7a32a110cd569778e126698ee5
cmorox <cristianmoro482@gmail.com>
2023-02-23 17:02:53 -0600

    Empezamos el 03 strings

--- 02_operators.py
+++ 02_operators.py
@@ -13,7 +13,7 @@ print("Hola" + "Python" + " Que tal?")
 print('Hola ' + str(5))
 
 
-### Operadores comparativos ####
+### comparative operators ####
 
 print(3 > 4)
 print(3 < 4)
@@ -21,4 +21,22 @@ print(3 >= 4)
 print(3 <= 4)
 print(3 == 4)
 print(3 != 4)
+print(3 > 4 == 2)
 
+
+
+print('Hola' > 'Python')
+print('Hola' < 'Python')
+print('Hola' >= 'Python')
+print('Hola' <= 'Python')
+print('Hola' == 'Python')
+print('Hola' != 'Python')
+
+
+### Logic operators ####
+
+print(3 > 4 and 'Hola' > 'Python')
+print(3 > 4 or 'Hola' > 'Python')
+print(3 < 4 and 'Hola' < 'Python')
+print(3 > 4 or 'Hola' < 'Python')
+print(not (3 > 4 ))

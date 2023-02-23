8e246d23117bf498eb0f003a14008d25f04e14ed
cmorox <cristianmoro482@gmail.com>
2023-02-22 22:21:49 -0600

    Seguimos en el 02 y cambie la fuente de vsc

--- 02_operators.py
+++ 02_operators.py
@@ -6,4 +6,19 @@ print(3 * 4)
 print(3 / 4)
 print(3 % 4)
 print(3 | 4)
+print(12 // 10)
+print(3 ** 4)
+
+print("Hola" + "Python" + " Que tal?")
+print('Hola ' + str(5))
+
+
+### Operadores comparativos ####
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
 

f151a632d34a6cfd283027f98082c7375644cd1f
cmorox <cristianmoro482@gmail.com>
2023-02-22 19:18:48 -0600

    Pruebaaa 1

---
+++ hello.py
@@ -0,0 +1,24 @@
+# This is  a comment: Hello Python
+# Our Hello word in Python
+print("Hello Python")
+print('Hello Python')
+
+
+
+"""
+This is a comment in many lines
+"""
+
+'''
+This is a comment in many lines too
+'''
+#   consult data type
+print(type('Soy un dato str')) #Str type Cadena de caracteres
+
+print(type(5)) #Int type Numero entero
+
+print(type(1.6)) #Float type Numero decimal
+
+print(type(5 + 1j)) #Complex type  Numero complejo
+
+print(type(True)) #Bool type  Boolea
